30a265fd202561852bff01dd6df9658d978e2eae
reshma <reshmasridhar0804@gmail.com>
2026-01-18 00:34:22 +0530

    dto request for schedule  is added

---
+++ src/main/java/com/busbooking/dto/request/ScheduleRequest.java
@@ -0,0 +1,52 @@
+package com.busbooking.dto.request;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ScheduleRequest {
+	private Long busId;
+    private String source;
+    private String destination;
+    private LocalDate journeyDate;
+    private LocalTime departureTime;
+    private LocalTime arrivalTime;
+	public Long getBusId() {
+		return busId;
+	}
+	public void setBusId(Long busId) {
+		this.busId = busId;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public LocalDate getJourneyDate() {
+		return journeyDate;
+	}
+	public void setJourneyDate(LocalDate journeyDate) {
+		this.journeyDate = journeyDate;
+	}
+	public LocalTime getDepartureTime() {
+		return departureTime;
+	}
+	public void setDepartureTime(LocalTime departureTime) {
+		this.departureTime = departureTime;
+	}
+	public LocalTime getArrivalTime() {
+		return arrivalTime;
+	}
+	public void setArrivalTime(LocalTime arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+    
+    
+
+}
